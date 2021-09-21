2a527600faecdf43e0d4c5bc277c61cfe667a870
Merlin Mathesius <mmathesi@redhat.com>
2021-09-21 17:00:06 -0500

    Added logging, command line parsing, and beginning of check logic
    Signed-off-by: Merlin Mathesius <mmathesi@redhat.com>

--- eln-check.py
+++ eln-check.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
 
+import click
+import datetime
 import json
+import logging
 import re
 import urllib.request
 
 from bs4 import BeautifulSoup as Soup
 
 
+logger = logging.getLogger(__name__)
+
+
 def get_composes(composes_url, name="Fedora-ELN", version="Rawhide"):
     """
-    composes_url: top level URL containing composes
+    Return list of available composes
 
-    returns: ordered list of composes, from newest to oldest
+    :param composes_url: top level URL containing composes
+    :param name: OS name for which to return composes
+    :param version: OS version for which to return composes
+    :return: ordered list of composes, from newest to oldest
     """
 
     compose_re = re.compile(
@@ -35,9 +44,13 @@ def get_composes(composes_url, name="Fedora-ELN", version="Rawhide"):
 
 def compose_status(composes_url, compose):
     """
-    returns: tuple(status, date) where
-        status is None, 'FINISHED', 'FINISHED_INCOMPLETE', etc.
-        and date is None or a string in the form of YYYYMMDD
+    Fetch the status of the specified compose.
+
+    :param composes_url: top level URL containing composes
+    :param compose: the compose for which to fetch the status
+    :return: 2-tuple of the form (status, date) where
+             status is None, 'FINISHED', 'FINISHED_INCOMPLETE', etc.
+             and date is None or a string in the form of YYYYMMDD
     """
 
     try:
@@ -62,20 +75,88 @@ def compose_status(composes_url, compose):
     return status, composedate
 
 
+@click.command()
+@click.option(
+    "--debug",
+    is_flag=True,
+    help="Output a lot of debugging information",
+    show_default=True,
+    default=False,
+)
+@click.option(
+    "--url",
+    help="Top level URL containing composes",
+    show_default=True,
+    default="https://odcs.fedoraproject.org/composes/production",
+)
+@click.option(
+    "--name",
+    help="OS name for which to check composes",
+    show_default=True,
+    default="Fedora-ELN",
+)
+@click.option(
+    "--version",
+    help="OS version for which to check composes",
+    show_default=True,
+    default="Rawhide",
+)
+@click.option(
+    "--input",
+    help="Input file containing status from previous check",
+    type=click.Path(exists=True, readable=True),
+)
+@click.option(
+    "--output",
+    help="Output file to contain status from this check",
+    type=click.Path(writable=True),
+)
+def cli(debug, url, name, version, input, output):
+    # alternate: --url "https://composes.stream.centos.org/production" --name "CentOS-Stream" --version "9"
+
+    if debug:
+        logging.basicConfig(format="%(asctime)s:%(levelname)s:%(name)s:%(message)s")
+        logger.setLevel(logging.DEBUG)
+        logger.debug("Debugging mode enabled")
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    now = datetime.datetime.now()
+    today = now.date()
+
+    logger.debug("Today is {}".format(str(today)))
+    logger.debug("Now is {}".format(now.strftime("%Y-%m-%d %H:%M:%S")))
+
+    latest_attempted = {"compose": None, "date": None}
+    latest_finished = {"compose": None, "date": None}
+    latest_incomplete = {"compose": None, "date": None}
+
+    logger.info(f"For {url = }, {name = }, {version = }")
+    composes = get_composes(url, name, version)
+    # Note: list of composes is ordered from newest to oldest
+    for c in composes:
+        logger.debug("Getting status for compose = {}".format(c))
+        status, date = compose_status(url, c)
+        if date is None:
+            logger.debug("No date, extracting from compose name {}".format(c))
+            date_re = re.compile(
+                "^{name}-{version}-(\d{{8}})\..*$".format(name=name, version=version)
+            )
+            match = re.search(date_re, c)
+            if match:
+                date = match.group(1)
+                logger.debug("Extracted date {} from compose name {}".format(date, c))
+            else:
+                logger.notice("Cannot extract date from compose name {}".format(c))
+                next
+
+        parsed_date = datetime.datetime.strptime(date, "%Y%m%d").date()
+        age = (today - parsed_date).days
+        logger.info(
+            "Compose {} status = {} date = {} age = {}".format(c, status, date, age)
+        )
+        # if latest_attempt["compose"] is None:
+
+
 if __name__ == "__main__":
-
-    url = "https://composes.stream.centos.org/production"
-    print(f"For {url =}:")
-    composes = get_composes(url, "CentOS-Stream", "9")
-    for c in composes:
-        print("Getting status for compose = {}".format(c))
-        status, date = compose_status(url, c)
-        print("status, date = {}, {}".format(status, date))
-
-    url = "https://odcs.fedoraproject.org/composes/production"
-    print(f"For {url =}:")
-    composes = get_composes(url, "Fedora-ELN", "Rawhide")
-    for c in composes:
-        print("Getting status for compose = {}".format(c))
-        status, date = compose_status(url, c)
-        print("status, date = {}, {}".format(status, date))
+    cli()
